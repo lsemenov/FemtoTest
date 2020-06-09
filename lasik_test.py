8f75fc3b0bf694bb0507f12a69737cc5a05e4f90
alex_tester <ra9ian@mail.ru>
2020-06-09 13:09:56 +0300

    'new'

---
+++ lasik_test.py
@@ -0,0 +1,42 @@
+from selenium import webdriver
+import allure
+import pytest
+import os
+
+
+
+@pytest.fixture()
+def test_setup():
+    global driver
+    os.startfile(r'C:\Users\_username_\PycharmProjects\testinf_Femo\driver\Winium.Desktop.Driver.exe')
+    driver = webdriver.Remote(command_executor='http://localhost:9999',
+                                       desired_capabilities={'app': r"C:\Users\_username_\Desktop\фемтовизум\сервис мануал\FemtoOld\2.8.5.24\FemtoVisum.exe"})
+    yield
+    driver.quit()
+
+
+@allure.feature('Ввод операцционных парамтеров ZLasik')
+@allure.story('Ввод допустимого операционного параметра "Диаметр"')
+@allure.severity('critical')
+def test_ui_zlasik(test_setup):
+
+    # Входим в меню выбора операции
+    window_operation = driver.find_element_by_class_name('#32770')     # Находим окно меню
+    button_operation = window_operation.find_element_by_id('1021')
+    button_operation.click()  # Находим кнопку операция
+
+    # выбор операции z-lasik
+    window_operation_menu = driver.find_element_by_class_name('#32770')
+    window_operation_menu.find_element_by_id('1015').click()
+
+    # Ввод операционных парамтеров
+    diameter = '9.1'
+
+    # диаметр
+    window_oper_param = driver.find_element_by_class_name('#32770')
+    window_oper_param.find_element_by_id('1126').send_keys(diameter)
+    driver.find_element_by_id('1015').click()
+    text = str(driver.find_element_by_id('1126').text)
+    assert text == diameter
+    driver.quit()
+
